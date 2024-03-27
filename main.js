270726d4a21bb9057e188071bbb83b9cc3670c06
fushigoroma <164196668+fushigoroma@users.noreply.github.com>
2024-03-27 12:14:31 -0300

    alura27/03

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ for(let i=0;i <botoes.length;i++){
         
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            texto[j].classList.remove("ativo");
+
         }
+       
         botoes[i].classList.add("ativo");
+        texto[i].classList.add("ativo");
     }
-}
+}const botoes = document.querySelectorAll(".contador");
+const textos = document.querySelectorAll("2024-10-05T00:00:00");
